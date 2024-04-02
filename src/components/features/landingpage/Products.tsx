fff22e25803c4e2cbe7e83b0bb7050b21e32e53b
hosein khaleghi <hoseinkhaleghi97@gmail.com>
2024-04-02 23:47:46 +0330

    patch 03

--- src/components/features/landingpage/Products.tsx
+++ src/components/features/landingpage/Products.tsx
@@ -1,18 +1,41 @@
+import { useState } from "react";
 import { list } from "../../../list";
 import styles from "./Products.module.css";
 function Products() {
+  // const platform = list.map((item)=>(item.platform))
+  const [platform, setPlatform] = useState("ps5");
   return (
-    <div>
+    <div className={styles.cont}>
       {list.map((item) => (
-        <div key={item.id} className={styles.cont}>
-          <div className={styles.card}>
-            <div className={styles.imgBx}>
-              <img src={item.img} />
-            </div>
-            <div className={styles.details}>
-              <h3>{item.name}</h3>
-              <h4>{item.email}</h4>
-            </div>
+        <div key={item.id} className={styles.card}>
+          <div className={styles.imgBx}>
+            <img
+              src={
+                platform === "ps5"
+                  ? item.imgps5
+                  : platform === "xbox"
+                  ? item.imgxbox
+                  : item.imgpc
+              }
+            />
+          </div>
+          <div className={styles.details}>
+            <h3>{item.name}</h3>
+            <h4>{item.email}</h4>
+            <ul className={styles.size}>
+              <li onClick={() => setPlatform("pc")}>
+                <img src={item.platform.pc} />
+              </li>
+              <li onClick={() => setPlatform("xbox")}>
+                <img src={item.platform.xbox} />
+              </li>
+              <li onClick={() => setPlatform("ps5")}>
+                <img src={item.platform.ps} />
+              </li>
+              {/* {platform.map((item)=>(
+                  <li><img src={item.pc} alt="" /></li>
+                ))} */}
+            </ul>
           </div>
         </div>
       ))}
--- src/list.tsx
+++ src/list.tsx
@@ -1,44 +1,78 @@
-import eldenps5 from './assets/images/eldenps5.jpg';
-import eldenps1 from './assets/images/eldenps5.jpg';
-import eldenps2 from './assets/images/eldenps5.jpg';
-import eldenps3 from './assets/images/eldenps5.jpg';
-import eldenps4 from './assets/images/eldenps5.jpg';
+import eldenps5 from "./assets/images/eldenps5.jpg";
+import eldenps1 from "./assets/images/eldenps5.jpg";
+import eldenps2 from "./assets/images/eldenps5.jpg";
+import eldenps3 from "./assets/images/eldenps5.jpg";
+import eldenps4 from "./assets/images/eldenps5.jpg";
+import eldenxbox from "./assets/images/eldenxbox.jpg";
+import eldenxpc from "./assets/images/eldenpc.jpg";
+import PC from "./assets/images/pc.jpg";
+import XBOX from "./assets/images/XBox.jpg";
+import PS from "./assets/images/ps.jpg";
 
-export const list =[
-    {
+export const list = [
+  {
     id: 1,
     name: "id labore ex et quam laborum",
     email: "Eliseo@gardner.biz",
-    img:eldenps5,
-    body: ""
+    imgps5: eldenps5,
+    imgxbox: eldenxbox,
+    imgpc:eldenxpc,
+    platform: {
+      pc: PC,
+      xbox: XBOX,
+      ps: PS,
     },
-    {
+  },
+  {
     id: 2,
     name: "quo vero reiciendis velit similique earum",
     email: "Jayne_Kuhic@sydney.com",
-    img:eldenps1,
-    body: ""
+    imgps5: eldenps1,
+    imgxbox: eldenxbox,
+    imgpc:eldenxpc,
+    platform: {
+      pc: PC,
+      xbox: XBOX,
+      ps: PS,
     },
-    {
+  },
+  {
     id: 3,
     name: "odio adipisci rerum aut animi",
     email: "Nikita@garfield.biz",
-    img:eldenps4,
-
-  "body": ""
+    imgps5: eldenps4,
+    imgxbox: eldenxbox,
+    imgpc:eldenxpc,
+    platform: {
+      pc: PC,
+      xbox: XBOX,
+      ps: PS,
     },
-    {
+  },
+  {
     id: 4,
     name: "alias odio sit",
     email: "Lew@alysha.tv",
-    img:eldenps3,
-    body: ""
+    imgps5: eldenps3,
+    imgxbox: eldenxbox,
+    imgpc:eldenxpc,
+    platform: {
+      pc: PC,
+      xbox: XBOX,
+      ps: PS,
     },
-    {
+  },
+  {
     id: 5,
     name: "vero eaque aliquid doloribus et culpa",
     email: "Hayden@althea.biz",
-    img:eldenps2,
-    body: ""
-    }
-]
+    imgps5: eldenps2,
+    imgxbox: eldenxbox,
+    imgpc:eldenxpc,
+    platform: {
+      pc: PC,
+      xbox: XBOX,
+      ps: PS,
+    },
+  },
+];
